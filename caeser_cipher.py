be025ff4aec8a43bc7d4f2ba7228f52dc024adf0
SACHIN BHATIA <40489515+sachinsbhatia@users.noreply.github.com>
2019-01-10 00:00:12 +0530

    Update caeser_cipher.py

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -2,6 +2,8 @@ alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 inpu= input("enter a string:")
 
+shift_input= int(input("Enter in the value you want to shift by:"))
+ 
 lengthh= len(inpu)
 
 outp= ""
@@ -9,8 +11,8 @@ outp= ""
 for i in range(lengthh):
     character = inpu[i]
     char_loc = alpha.find(character)
-    new_loc = char_loc + 3;
-    outp += alpha[new_loc]
+    new_loc = (char_loc + shift_input)%26;
+    outp = outp + alpha[new_loc]
 
 
 print("encrypted text is:", outp)
